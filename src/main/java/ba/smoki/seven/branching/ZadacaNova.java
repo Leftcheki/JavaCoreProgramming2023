eac2bb771d3255fdb6dd6dd6df8de8eaea226bd7
PC <majlomerk@gmail.com>
2023-11-24 00:03:56 +0100

    NovaZadaca

--- src/main/java/ba/smoki/seven/branching/ZadacaNova.java
+++ src/main/java/ba/smoki/seven/branching/ZadacaNova.java
@@ -12,5 +12,21 @@ package ba.smoki.seven.branching;
 public class ZadacaNova {
     public static void main(String[] args) {
 
+        String recenica = "petar pan je u pantalonama pojeo panprike i zato je pan petar panova u pantalonama sa pancirima";
+        String rijec = "pan";
+        int panCount=0;
+        int granica = recenica.length()-rijec.length();
+        BROJANJE: for(int i = 0; i<=granica; i++){
+            for(int j = 0; j<rijec.length(); j++){
+                char slovoRecenice = recenica.charAt(i+j);
+                char slovoRijeci = rijec.charAt(j);
+                if(slovoRecenice != slovoRijeci){
+                    continue BROJANJE;
+                }
+            }
+            panCount++ ;
+        }
+        String message = "Rijec pan se nalazi " + panCount + " puta u recenici.";
+        System.out.println(message);
     }
 }
